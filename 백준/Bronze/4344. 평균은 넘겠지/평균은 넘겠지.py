dc30cd62bdd0bfa1d17d3fde182dd4f52ca30eff
최동우 <90435431+dongwoochoi@users.noreply.github.com>
2023-02-08 23:31:25 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,11 @@
+C = int(input())
+
+for i in range(C):
+    num = list(map(int, input().split()))
+    avg = sum(num[1:])/num[0]
+    cnt = 0
+    for i in range(1, len(num)):
+        if num[i] > avg:
+            cnt += 1
+            
+    print('%.3f' % (cnt / num[0] * 100) + '%')
